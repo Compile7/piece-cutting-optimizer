92ad277f572515d2090d301427efe6b408a9d357
muskansoninmh <muskansoninmh1@gmail.com>
2023-02-10 18:50:30 +0530

    Added Comments in Algorithm

--- algorithm/algorithm.js
+++ algorithm/algorithm.js
@@ -1,3 +1,4 @@
+// function to convert object to an array as we will get input in {size:20, qty:2} => [20,20]
 const converObjectIntoArray = ( objArr) => {
     let finalArr = [];
     let Arr = []
@@ -8,18 +9,19 @@ const converObjectIntoArray = ( objArr) => {
     return finalArr;
 }
 
+// function which returns the cuts(size of cut and the index of cut in array )
+//  info so that we get minimum waste 
+//  input we give is the array for cuts and the length of pipe/rod
 const cutsinSinglePipe = (arr, rodLength) => {
     let min = rodLength; 
     let cut = 0;
     let index = 0;
     for (let j in arr) {
         if(rodLength >= arr[j] && min >= rodLength-arr[j] ){
-
             min = rodLength-arr[j];
             cut = arr[j];
             index = j;
         } 
-
     }
     const obj = {cut, index}
     return obj;
@@ -27,8 +29,14 @@ const cutsinSinglePipe = (arr, rodLength) => {
 
 
 
-
-const pieceCuttingAlgo = (cuts, stocks) => {
+//  Main function for cut piece Algo
+// Inputs 
+// 1. Array of cuts [{size: 10, qty:1}]
+// 2. Array of Stocks [{size: 10, qty:1}]
+// Output will get of one object which contain 2 Array 
+// 1. Contains subArrays for each stock 
+// 2. Contains Arrays of cuts which does not fit if stock is not enough otherwise null
+const pieceCutting1DAlgo = (cuts, stocks) => {
     let finalCuts =[];
     let totalSize = 0;
     let cutsInEachPipe = [0]; 
@@ -48,12 +56,13 @@ const pieceCuttingAlgo = (cuts, stocks) => {
             totalCutInEachPipe = 0;
             cutsArr = cutsLeft;
             
+            //for the creation of subArray 
+
             if(i!=0){
                 finalCuts.push(cutsInEachPipe)
                 cutsInEachPipe = [0]
             }
         }
-
 
         for (let j =0; j <= cutsArr.length; j++ ) {
             if(totalSize >= cutsArr[j]){
@@ -67,7 +76,6 @@ const pieceCuttingAlgo = (cuts, stocks) => {
            
         }
     }
-    let unabletofit = [] 
 
     const finalObject = {
         finalCuts,
@@ -76,11 +84,22 @@ const pieceCuttingAlgo = (cuts, stocks) => {
     return finalObject;
 
 }
-// console.log(pieceCuttingAlgo(
+
+
+
+// Example 
+// pieceCutting1DAlgo(
 //     [
 //         {size: 45, qty:1},
 //         {size: 25, qty:1},
 //         {size: 20, qty:3},
 //         {size: 15, qty:2},
-//         {size: 35, qty:1}]
-//     , [{size: 89, qty:3}]));
+//         {size: 35, qty:1}
+// ],
+//      [{size: 89, qty:2}]
+// );
+// Output:-
+// {
+//     finalCuts: [ [ 0, 45, 80 ], [ 0, 25, 45, 65, 85 ] ],
+//     unabletofit: [ 15, 15 ]
+//   }
