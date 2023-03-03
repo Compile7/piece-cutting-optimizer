a85fe17d17a387e36cdd00a6580a8b23cc5d62d2
muskansoninmh <muskansoninmh1@gmail.com>
2023-03-03 10:55:35 +0530

    commented cors part

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ const httpServer = require("http").createServer();
 
 const port = process.env.PORT || 3002
 const oneDAlgo = require('../algorithm/1Dalgorithm');
-const io = require("socket.io")(httpServer, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
-});
+const io = require("socket.io")(httpServer
+  // , {
+  // cors: {
+  //   origin: "http://localhost:3000",
+  // },}
+);
 
 io.on("connection", (socket) => {
   socket.on("message", oneDAlgoResult);
